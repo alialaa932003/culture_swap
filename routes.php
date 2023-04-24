d7a8b6bf0b473d549f7b9f64c42c506f49176909
shehab <shehabwaleed2010@gmail.com>
2023-04-24 19:06:50 +0200

    Modify routes file

--- routes.php
+++ routes.php
@@ -1,13 +1,4 @@
 <?php
-// return [
-
-//         '/'=>base_path("controller/index.php"),
-//         '/about'=>base_path("controller/about.php"),
-//         '/notes'=>base_path("controller/notes/index.php"),
-//         '/note'=>base_path("controller/notes/show.php"),
-//         '/notes/create'=>base_path("controller/notes/create.php"),
-//         '/contact'=>base_path("controller/contact.php")
-//     ];
 
     $router->get ('/culture_swap/','controller/index.php');
     $router->get ('/culture_swap/faq','controller/faq.php');
