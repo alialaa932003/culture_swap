1e2da146349e0db867ebf82bebe5fdf2a468c351
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2023-04-24 13:57:05 +0200

    home page

--- views/index.view.php
+++ views/index.view.php
@@ -3,28 +3,38 @@
 <html lang="en">
 <head>
     <?php
-        require base_path("views/partials/head.view.php");
-    ?>
-    <!-- عشان عارف انك حمار -->
-    <!--  put your css file here -->
+require base_path("views/partials/head.view.php");
+?>
+
+    <link rel="stylesheet" href="public/assets/css/home.css">
     <title>home</title>
 </head>
 <body>
-    <?php
-        require base_path("views/partials/nav.view.php");
-    ?>
-
-    <p>sadas</p>
-
-
-
 
     <?php
-        require base_path("views/partials/footer.view.php");
-    ?>
+require base_path("views/partials/nav.view.php");
+?>
+
+
+    <header class="section-padding">
+        <div class="container">
+            <div class="headerContent">
+            <h1>Travel differently, connect globally</h1>
+        <p> The largest and safest community for cultural exchange, working holidays and volunteering in 170 countries.
+        </p>
+        </div>
+        </div>
+
+    </header>
+
+
     <?php
-        require base_path("views/partials/scripts.view.php");
-    ?>
+require base_path("views/partials/footer.view.php");
+?>
+    <?php
+require base_path("views/partials/scripts.view.php");
+?>
+
 
     <!-- عشان عارف انك بهيم -->
     <!-- حط ملف الجافاسكربت بتاعك هنا -->
--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -1,4 +1,33 @@
-<h1>
+<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">culture swap</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav  ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">how it is work</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">support</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">community</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">favourites</a>
+        </li>
+        <li class="nav-item">
+          <a class=" navJoinBtn main-btn" href="#">join now</a>
+        </li>
 
-    navbar
-</h1>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
