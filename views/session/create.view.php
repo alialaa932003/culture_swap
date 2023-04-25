cacb4d120024d2d8f5d4e61fac533dadc037db9a
shehab <shehabwaleed2010@gmail.com>
2023-04-25 16:28:59 +0200

    Prepare the structure of login page

---
+++ views/session/create.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  require base_path("views/partials/head.view.php");
+  ?>
+  <!-- عشان عارف انك حمار -->
+  <!--  put your css file here -->
+  <link rel="stylesheet" href="/culture_swap/public/assets/css/login/login.css">
+  <title>login</title>
+</head>
+
+<body>
+  <?php
+  require base_path("views/partials/nav.view.php");
+  ?>
+
+  <p>sadas</p>
+
+
+
+
+  <?php
+  require base_path("views/partials/footer.view.php");
+  ?>
+  <?php
+  require base_path("views/partials/scripts.view.php");
+  ?>
+
+  <!-- عشان عارف انك بهيم -->
+  <!-- حط ملف الجافاسكربت بتاعك هنا -->
+</body>
+
+</html>
