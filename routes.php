bb9f47b38e3c6fc3ad6e17625cf709f4807cfc07
shehab <shehabwaleed2010@gmail.com>
2023-04-25 05:23:30 +0200

    Adding the basic structure for signup pages

--- routes.php
+++ routes.php
@@ -3,6 +3,8 @@
     $router->get ('/culture_swap/','controller/index.php');
     $router->get ('/culture_swap/faq','controller/faq.php');
     $router->get ('/culture_swap/about','controller/about.php');
+    $router->get ('/culture_swap/signup/traveller','controller/signup/traveller.php');
+    $router->get ('/culture_swap/signup/host','controller/signup/host.php');
     $router->get ('/culture_swap/template','controller/template.php');
 
 
