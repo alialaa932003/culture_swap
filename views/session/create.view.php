af18e4f3649fe3edaf60e1d51835faa6716cd03c
shehab <shehabwaleed2010@gmail.com>
2023-04-25 18:19:16 +0200

    Finish Login page

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -16,10 +16,24 @@
   require base_path("views/partials/nav.view.php");
   ?>
 
-  
+
   <div class="login-section section-padding">
     <div class="container">
       <div class="login-form">
+
+        <div class="logo">
+          <img src="public/assets/imgs/login/logo.png">
+        </div>
+
+        <div class="form-content">
+          <form method="POST" class="form">
+            <input type="text" class="username-input" name="username" placeholder="Username" required>
+            <input type="password" class="password-input" name="password" placeholder="Password" required>
+            <label class="remember-pass"><input type="checkbox" name="Remember-password">Remember Password</label>
+            
+            <button type="submit" class="submit main-btn btn">Log In</button>
+          </form>
+        </div>
 
       </div>
     </div>
