17529d1a0ce8a6b7157c7876f62b7f6889022573
shehab <shehabwaleed2010@gmail.com>
2023-04-25 06:40:50 +0200

    add home and about links to navbar

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -7,10 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav  ml-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/culture_swap/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">how it is work</a>
+          <a class="nav-link" href="/culture_swap/about">how it is work</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">support</a>
