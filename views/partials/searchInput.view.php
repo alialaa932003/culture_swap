b6b084aebfa8ae3c17b138a7c01cbd5fb2119177
salahcst344 <salahcst344@gmail.com>
2023-04-29 18:00:41 +0300

    Add search input and his styles

---
+++ views/partials/searchInput.view.php
@@ -0,0 +1,16 @@
+<div class="searchInputContainer py-4">
+  <div class="container">
+    <form class="bg-white">
+      <input 
+        type="text" 
+        name="searchQ" 
+        class="searchInput form-control" 
+        placeholder="Search for what you want" 
+      />
+
+      <button type="submit" class="btn search-btn">
+        <i class="fa-sharp fa-solid fa-magnifying-glass fa-xl" style="color: inherit;"></i>
+      </button>
+    </form>
+  </div>
+</div>
