9ce27312a69ef0f43006d926e64dc44e7ca4caab
shehab <shehabwaleed2010@gmail.com>
2023-04-29 23:06:54 +0300

    Adding the traveller class (Not finished yet)

--- core/Classes/Traveller.php
+++ core/Classes/Traveller.php
@@ -9,7 +9,7 @@ class Traveller extends User
   // Data fields
   
   private $service;
-  private $froendsIds = [];
+  private $friendsIds = [];
   private $favHostsIds = [];
 
   // Constructors
@@ -17,7 +17,7 @@ class Traveller extends User
   {
     // Default constructor
   }
-
+  
   public function __construct1($username, $password, $type)
   {
     // Constructor with three parameters
@@ -25,7 +25,7 @@ class Traveller extends User
     $this->password = $password;
     $this->type = $type;
   }
-
+  
   public function __construct2($name, $username, $password, $type, $email, $phoneNumber, $country, $service)
   {
     // Constructor with eight parameters
@@ -37,6 +37,68 @@ class Traveller extends User
     $this->phoneNumber = $phoneNumber;
     $this->country = $country;
     $this->service = $service;
+  }
+  
+  // Methods
+  public function getService(){
+    return $this->service;
+  }
+  
+  public function setService($service){
+    $this->service = $service;
+  }
+  
+  public function getFavHostsIds(){
+    return $this->favHostsIds;
+  }
+  
+  public function getFriendsIds(){
+    return $this->friendsIds;
+  }
+  public function addFavHostId($hostId){
+    array_push($this->favHostsIds, $hostId);
+  }
+  
+  public function removeFavHostsId($hostId){
+    $index = array_search($hostId, $this->favHostsIds);
+    if ($index !== false) {
+      unset($this->favHostsIds[$index]);
+      return true;
+    }
+    return false;
+  }
+
+  public function makeReview($rate , $hostId){
+    $review = new Review($hostId, $this->id, $rate);
+    return $review;
+  }
+
+  public function removeReview($reviewId){
+
+  }
+
+  public function makeReservation($hostId){
+    $reservation = new Reservation($this->id, $hostId, 0); // 0 here is pending
+    return $reservation;
+  }
+
+  public function cancelReservation(){
+
+  }
+
+  public function addFriend($friendId){
+    array_push($this->friendsIds, $friendId);
+    $friend = new Friend($friendId, $travellerId);
+    return $friend;
+  }
+
+  public function removeFriend($friendId){
+    $index = array_search($friendId, $this->friendsIds);
+    if ($index !== false) {
+      unset($this->friendsIds[$index]);
+      return true;
+    }
+    return false;
   }
 
 
