8f01a6877224104687adc6ac21c35dfc3910936a
shehab <shehabwaleed2010@gmail.com>
2023-05-03 01:48:57 +0300

    Adding reservationDB class

--- controller/faq.php
+++ controller/faq.php
@@ -1,14 +1,4 @@
 <?php
 
-use core\Classes\DB\ReservationDB;
-
-
-ReservationDB::update([
-  'id' => '27',
-  'key' => 'traveller_id',
-  'value' => '6'
-]);
-dd(ReservationDB::getAll());
-
 
 require base_path("views/faq.view.php");
