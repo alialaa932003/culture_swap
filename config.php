3b86ffcc0a397242e491c15bee042b024675245e
salahcst344 <salahcst344@gmail.com>
2023-05-02 22:03:01 +0300

    Update DB name

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
-    return[
-            'database'=>[
-
-                'host'=>'localhost',
-            'port'=>'3306',
-            'dbname'=>'learnphp',
-            'charset'=>'utf8mb4'
-            ],
-            'base_urll'=>'http://localhost/ali',
-        ];
-?>
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '3306',
+        'dbname' => 'culture_swap',
+        'charset' => 'utf8mb4'
+    ],
+    'base_urll' => 'http://localhost/ali',
+];
