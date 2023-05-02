453b6fa73ad167f1dae2023530db498f5c57e1f4
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2023-05-02 23:10:14 +0300

    notification

--- controller/index.php
+++ controller/index.php
@@ -3,5 +3,5 @@ $ASSET_URL = "/culture_swap/public/";
 
 use core\Classes\DB\NotificationDB;
 
-dd((new NotificationDB())->getAll());
+dd(NotificationDB::getAll());
 require base_path("views/index.view.php");
