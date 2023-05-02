da2ad0938e57250ef131009431698cc6751ee9a6
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2023-05-02 22:41:07 +0300

    database

--- core/Classes/DB/NotificationDB.php
+++ core/Classes/DB/NotificationDB.php
@@ -18,7 +18,7 @@ class NotificationDB
     {
 
         $this->dbref->query(
-            "INSERT INTO TABLE [notification] (sender_id,reciever_id,[content],[action]) 
+            "INSERT INTO TABLE notification (sender_id,reciever_id,content,action) 
             
             values(:sender_id,:reciever_id,:contentt,:actionn)",
             [
@@ -33,8 +33,9 @@ class NotificationDB
     public function delete($id)
     {
         $this->dbref->query(
-            "DELETE FROM [notification] WHERE id=:id",
+            "DELETE FROM notification WHERE id=:id",
             [
+
                 'id' => $id
             ]
         );
@@ -42,23 +43,21 @@ class NotificationDB
     public function getAll()
     {
 
-        return $this->dbref->query("SELECT * FROM post ")->get();
+        return $this->dbref->query("SELECT * FROM notification INNER JOIN interaction on interaction.code = notification.id ")->get();
     }
     public function search($str, $offset, $limit)
     {
         return $this->dbref->query(
-            "SELECT * FROM post WHERE title LIKE '%:str%' OR [content] like '%:str%' ORDER BY id desc  LIMIT :limit OFFSET :offset ",
+            "SELECT * FROM post WHERE title LIKE '%:str%' OR content like '%:str%' ORDER BY id desc  LIMIT :limit OFFSET :offset ",
             ['limit' => $limit, 'offset' => $offset, 'str' => $str]
         )->get();
     }
     public function getOne($id)
     {
-        $post = $this->dbref->query("SELECT * FROM post WHERE id = :id", ['id' => $id])->findOrFail();
+        $notification = $this->dbref->query("SELECT * FROM notification WHERE id = :id", ['id' => $id])->findOrFail();
         if (empty($post)) {
             return [];
         }
-        $comments = $this->dbref->query("SELECT [comment].* ,first_name,last_name ,country,profile_picture,type FROM [comment] INNER JOIN user_info on user_info.id = [comment].user_id  WHERE  post_id = :id ", ['id' => $post['id']])->findOrFail();
-        $post['comments'] = $comments;
-        return $post;
+        return $notification;
     }
 }
