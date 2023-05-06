1eee8a63e0a5961456c4206b22069aacf5aab187
shehab <shehabwaleed2010@gmail.com>
2023-05-06 16:20:28 +0300

    Organize routes

--- routes.php
+++ routes.php
@@ -7,17 +7,15 @@ $router->post('/culture_swap/posts/store', 'controller/community/store.php');
 $router->get('/culture_swap/faq', 'controller/faq.php');
 $router->get('/culture_swap/about', 'controller/about.php');
 
+//! Signup Requests
 $router->get('/culture_swap/signup/traveller', 'controller/signup/traveller/createTraveller.php')->only('guest');
 $router->post('/culture_swap/signup/traveller', 'controller/signup/traveller/storeTraveller.php');
-
 $router->get('/culture_swap/signup/traveller_vip', 'controller/signup/traveller/createTravellerVip.php')->only('guest');
 $router->post('/culture_swap/signup/traveller_vip', 'controller/signup/traveller/storeTravellerVip.php');
-
 $router->get('/culture_swap/signup/host', 'controller/signup/host/createHost.php')->only('guest');
 $router->post('/culture_swap/signup/host', 'controller/signup/host/storeHost.php');
-
+//! Login Requests
 $router->get('/culture_swap/login', 'controller/session/create.php')->only('guest');
-$router->get('/culture_swap/template', 'controller/template.php');
 
 $router->get('/culture_swap/test', 'controller/test.php');
 
