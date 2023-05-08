0f8da7b7b2a9e1a801798108b8fb6d303446d0df
salahcst344 <salahcst344@gmail.com>
2023-05-08 15:53:20 +0300

    Fix styleing

--- views/partials/grid.view.php
+++ views/partials/grid.view.php
@@ -1,10 +1,10 @@
 <div class=<?= $gridContainerClass ?? "container" ?>>
   <?= $createHead ? $createHead($headData) : null ?>
 
-  <div class=<?= $rowContainerClass ?? "row" ?>>
-      <?php 
-        foreach ($cardsData as $cardData)
-          Components::createCard($cardData, $cardClasses)
-      ?>
+  <div class=<?= $rowContainerClass ?? 'row' ?> style='justify-content:center'>
+    <?php
+    foreach ($cardsData as $cardData)
+      Components::createCard($cardData, $cardClasses)
+    ?>
   </div>
 </div>
