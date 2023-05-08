a229742a3bb91f5f435661a3371fb27606d9c625
salahcst344 <salahcst344@gmail.com>
2023-05-08 16:24:52 +0300

    Make search functional

--- controller/travelers.php
+++ controller/travelers.php
@@ -1,5 +1,7 @@
 <?php
 $ASSET_URL = "/culture_swap/public/";
-$cardsData = fetchCardData($_GET);
+
+$page = $_GET['page'] ?? 0;
+$cardsData = fetchTravelersCardData($_GET, $page);
 
 require base_path("views/travelers.view.php");
--- views/travelers.view.php
+++ views/travelers.view.php
@@ -1,35 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<?php
-			require base_path("views/partials/head.view.php");
+	require base_path("views/partials/head.view.php");
 	?>
 	<title>home</title>
-	<link rel="stylesheet" href=<?="{$ASSET_URL}/assets/css/search/searchInput.css"?>>
+	<link rel="stylesheet" href=<?= "{$ASSET_URL}/assets/css/search/searchInput.css" ?>>
 </head>
+
 <body>
 	<?php
-		require base_path("views/partials/nav.view.php");
+	require base_path("views/partials/nav.view.php");
 	?>
 	<div style="padding: 1rem"></div>
 	<?php
-		require base_path("views/partials/searchInput.view.php")
+	require base_path("views/partials/searchInput.view.php")
 	?>
 
-	<div class="mt-5 py-5 d-flex">
+	<div class="container">
+		<div class="mt-5 py-5 d-flex justify-content-between" style="gap:4.2rem">
+			<?php
+			require base_path("views/partials/filters.view.php");
+			?>
+
+			<?php Components::createCardsGrid($cardsData, false) ?>
+		</div>
+	</div>>
 
 	<?php
-		Components::createCardsGrid($cardsData)
+	require base_path("views/partials/footer.view.php");
 	?>
-
-	</div>
-
 	<?php
-			require base_path("views/partials/footer.view.php");
-	?>
-	<?php
-			require base_path("views/partials/scripts.view.php");
+	require base_path("views/partials/scripts.view.php");
 	?>
 </body>
+
 </html>
