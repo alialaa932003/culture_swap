14b304ecb40688b22ad66f3e0a03a670abaf5f5b
salahcst344 <salahcst344@gmail.com>
2023-05-08 15:53:04 +0300

    Fix var naming

--- views/partials/card.view.php
+++ views/partials/card.view.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $starFull = "<i class=\"fa-solid fa-star\"></i>";
 $starEmpty = "<i class=\"fa-regular fa-star\"></i>";
 
@@ -6,31 +6,31 @@ $heartFull = "<i class=\"fa-solid fa-heart\"></i>";
 $heartEmpty = "<i class=\"fa-regular fa-heart\"></i>";
 ?>
 
-<div class=<?=$cardContainerClass ?? "col-xl-4 col-md-6"?>>
+<div class=<?= $cardContainerClass ?? "col-xl-4 col-md-6" ?>>
   <div class="myCard">
     <div class="cardImage">
-      <img src=<?=$img?> alt=<?="host's place {$country}"?>>
+      <img src=<?= $cover_img ?> alt=<?= "host's place {$country}" ?>>
       <button class="love" onclick="$toggleFav()">
-          <span>
-            <?=$isFav ? $heartFull : $heartEmpty?>
-          </span>
+        <span>
+          <?= $isFav ? $heartFull : $heartEmpty ?>
+        </span>
       </button>
     </div>
     <div class="cardContent">
       <div class="cardCountry">
         <span class="countryIcon"><i class="fa-solid fa-location-dot"></i></span>
-        <span class="countryText"><?=$country?></span>
+        <span class="countryText"><?= $country ?></span>
       </div>
-      <h3><?=$title?></h3>
+      <h3><?= "{$first_name} {$last_name}" ?></h3>
       <div class="cardDetails">
-        <a href=<?="/profile?id={$hostId}"?> class="second-btn">view profile</a>
+        <a href=<?= "/profile?id={$Id}" ?> class="second-btn">view profile</a>
         <div class="rate">
-          <?php 
-            $i = 0;
-            for($i; $i < $rating; $i++)
-              echo "<span>{$starFull}</span>";
-            for($i; $i < 5; $i++)
-              echo "<span>{$starEmpty}</span>";
+          <?php
+          $i = 0;
+          for ($i; $i < $Rate_average; $i++)
+            echo "<span>{$starFull}</span>";
+          for ($i; $i < 5; $i++)
+            echo "<span>{$starEmpty}</span>";
           ?>
         </div>
       </div>
