44e896c1c50edf59151badd4fb1b94b694e625e2
salahcst344 <salahcst344@gmail.com>
2023-05-08 15:52:15 +0300

    Make fetchHostCardsData works

--- core/functions.php
+++ core/functions.php
@@ -1,168 +1,70 @@
 <?php
 
-
+use core\Classes\Host;
 use core\Response;
-function dd($value){
+
+function dd($value)
+{
     echo "<pre>";
     var_dump($value);
 
     echo "</pre>";
     die();
 }
-function base_path($path){
+function base_path($path)
+{
     return BASE_PATH . $path;
 }
 
-function abort($code=404){
+function abort($code = 404)
+{
     http_response_code($code);
 
-        require base_path("views/error$code.php");
-        die();
+    require base_path("views/error$code.php");
+    die();
 }
 
-function authorize($condition,$status =Response::FORBIDDEN){
-    if(!$condition){
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (!$condition) {
         abort($status);
     }
 }
 
-function fetchCardData($params) {
-    $cardsData = [
-        [
-            'img' => "public/assets/imgs/home/header5.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 4,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header4.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 5,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header2.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 1,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header2.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 3,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header5.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 0,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header4.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 4,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header4.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 5,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header2.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 2,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header5.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 3,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header2.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 4,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header4.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 1,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
-        [
-            'img' => "public/assets/imgs/home/header5.webp",
-            'country' => "norway",
-            'title' => "Beautiful smallholding on the island of Engeløya, Norway",
-            'rating' => 4,
-            'hostId' => 1,
-            'isFav' => true,
-            'toggleFav' => function () {},
-        ],
+function fetchHostsCardData($params, $page)
+{
+    $filters = [
+        'first_name' => $params['searchQ'] ?? '',
+        'last_name' => $params['searchQ'] ?? '',
+        'country' => $params['searchQ'] ?? '',
+        'needIds' => $params['needIds'],
+        'startRate' => $params['startRate'] ?? 0,
+        'endRate' => $params['endRate'] ?? 5
     ];
+    $limit = Components::getCardsPerPageLimit();
+    $cardsData = Host::search($filters, $page, $limit);
 
     return $cardsData;
 }
 //////////////////////////////////////////////////////////////
- function  login($user)
+function  login($user)
 {
-    
+
     session_start();
-  $_SESSION['user'] = [
-    'type' => $user->getType(),
-    'email' => $user->getEmail(),
-    'username' => $user->getUserName(),
-    'id' => $user->getId(),
-    'country' => $user->getCountry(),
-  ];
-  session_regenerate_id(true); // To have a high security
+    $_SESSION['user'] = [
+        'type' => $user->getType(),
+        'email' => $user->getEmail(),
+        'username' => $user->getUserName(),
+        'id' => $user->getId(),
+        'country' => $user->getCountry(),
+    ];
+    session_regenerate_id(true); // To have a high security
 }
 
 
-function  logout(){
-  $_SESSION = []; // create our session super global
- 
+function  logout()
+{
+    $_SESSION = []; // create our session super global
+
     session_destroy();
 }
-
-
-
-
-?>
