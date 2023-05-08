0f4483897b5a74ff46f31bd16763cff484b7f8f8
salahcst344 <salahcst344@gmail.com>
2023-05-08 16:24:27 +0300

    Make components more genaric

--- Components.php
+++ Components.php
@@ -9,14 +9,14 @@ class Components
     return Components::$CARDS_PER_PAGE;
   }
 
-  public static function createCard($cardData, $cardClasses)
+  public static function createCard($cardData, $cardClasses, $isHost = true)
   {
     extract($cardData);
     extract($cardClasses);
     require base_path("views/partials/card.view.php");
   }
 
-  public static function createCardsGrid($cardsData, $gridClasses = [], $cardClasses = [], $createHead = null)
+  public static function createCardsGrid($cardsData, $isHost = true, $gridClasses = [], $cardClasses = [], $createHead = null)
   {
     extract($gridClasses);
     require base_path("views/partials/grid.view.php");
