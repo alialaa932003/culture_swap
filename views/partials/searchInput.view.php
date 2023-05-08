53115d92e3100e3f09d8bcc5962a755e236fc8c6
salahcst344 <salahcst344@gmail.com>
2023-05-08 15:53:45 +0300

    Make search input more friendlly

--- views/partials/searchInput.view.php
+++ views/partials/searchInput.view.php
@@ -1,12 +1,7 @@
 <div class="searchInputContainer py-4">
   <div class="container">
     <form class="bg-white">
-      <input 
-        type="text" 
-        name="searchQ" 
-        class="searchInput form-control" 
-        placeholder="Search for what you want" 
-      />
+      <input type="text" name="searchQ" value="<?= $_GET['searchQ'] ? $_GET['searchQ']  : null ?>" class="searchInput form-control" placeholder="Search for what you want" />
 
       <button type="submit" class="btn search-btn">
         <i class="fa-sharp fa-solid fa-magnifying-glass fa-xl" style="color: inherit;"></i>
