f68f82d0fea23032162b3ffd63508cba040f68f8
shehab <shehabwaleed2010@gmail.com>
2023-05-09 20:13:01 +0300

    Adding services filter

--- controller/hosts.php
+++ controller/hosts.php
@@ -2,13 +2,16 @@
 $ASSET_URL = "/culture_swap/public";
 
 $page = $_GET['page'] ?? 0;
+
+//! Services filter logic
 $needIds = [];
 foreach($_GET['services'] as $service){
   array_push($needIds, $service);
 };
 $_GET['needIds'] = implode(',', $needIds);
+
+
+
 $cardsData = fetchHostsCardData($_GET, $page);
-
-
 
 require base_path("views/hosts.view.php");
