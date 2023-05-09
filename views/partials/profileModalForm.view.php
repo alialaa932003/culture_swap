7333c5b7e5672293996c07c2f3760f8f81b70802
salahcst344 <salahcst344@gmail.com>
2023-05-09 23:15:04 +0300

    INIT

---
+++ views/partials/profileModalForm.view.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="profileForms" tabindex="-1" aria-labelledby="profileFormsLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header px-5">
+        <h5 class="modal-title" id="profileFormsLabel" style="font-size: 3.2rem;">Edit your data</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="second-btn" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="main-btn">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
