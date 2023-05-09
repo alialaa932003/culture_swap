b17c2743731a8c96362f42151745a80edc293f36
salahcst344 <salahcst344@gmail.com>
2023-05-09 16:18:25 +0300

    Test

--- controller/test.php
+++ controller/test.php
@@ -11,12 +11,21 @@ ini_set('error_reporting', 1);
 
 use core\Classes\TravellerVip;
 
-dd(HostDB::search([
-  'needIds' => "8,2",
-  'startRate' => 0,
-  'endRate' => 4
-], 0, 30));
-
-
-// $traveller->getOne(6);
-// dd($traveller->delete(5));
+dd(HostDB::add([
+  'first_name' => 'asfaf',
+  'last_name' => 'asfwfasfsaf',
+  'user_name' => 'asfsaf5477',
+  'email' => '5asf4as@g.com',
+  'type' => 1,
+  'phone_num' => '5454112',
+  'profile_img' => '',
+  'cover_img' => '',
+  'country' => 'USA',
+  'password' => '451412',
+  'status' => 'Active',
+  'Description' => '452154',
+  'Rate_average' => 3,
+  'travelers_num' => 9,
+  'location' => 'los815',
+  'needs' => [1, 2]
+]));
