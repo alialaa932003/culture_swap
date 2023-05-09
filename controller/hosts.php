c7fa616caa5bad756f3d3987fc2bb11676eb0065
shehab <shehabwaleed2010@gmail.com>
2023-05-09 20:12:26 +0300

    Adding services filter

--- controller/hosts.php
+++ controller/hosts.php
@@ -2,7 +2,13 @@
 $ASSET_URL = "/culture_swap/public";
 
 $page = $_GET['page'] ?? 0;
+$needIds = [];
+foreach($_GET['services'] as $service){
+  array_push($needIds, $service);
+};
+$_GET['needIds'] = implode(',', $needIds);
 $cardsData = fetchHostsCardData($_GET, $page);
+
 
 
 require base_path("views/hosts.view.php");
--- views/partials/filters.view.php
+++ views/partials/filters.view.php
@@ -1,4 +1,5 @@
-<aside style="gap: 3.2rem;width:10%">
+<aside style="gap: 3.2rem;width:15%;">
+
   <form class="d-flex flex-column" style="gap: 3.2rem;">
     <div style=<?= $isHost ? "display: none;" : '' ?>>
       <h4 class='mb-3'>Rating</h4>
@@ -24,4 +25,65 @@
       <button type="submit" class="main-btn">Filter</button>
     </div>
   </form>
+
+  <form class="d-flex flex-column" style="gap: 3.2rem;">
+    <div style=<?= $isHost ? "display: none;" : '' ?>>
+      <h4 class='mb-3'>Countries</h4>
+    
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='4OrMore' name='startRate' value='4' />
+        <label class='form-check-label' for='4OrMore'>4 or more</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='4OrMore' name='startRate' value='4' />
+        <label class='form-check-label' for='4OrMore'>4 or more</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='4OrMore' name='startRate' value='4' />
+        <label class='form-check-label' for='4OrMore'>4 or more</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='4OrMore' name='startRate' value='4' />
+        <label class='form-check-label' for='4OrMore'>4 or more</label>
+      </div>
+
+    <div>
+      <button type="submit" class="main-btn">Filter</button>
+    </div>
+  </form>
+
+  <form class="d-flex flex-column" style="gap: 3.2rem;">
+    <div style=<?= $isHost ? "display: none;" : '' ?>>
+      <h4 class='mb-3'>Services</h4>
+
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='1' name='services[]' value='1' />
+        <label class='form-check-label' for='1'>Animals & Farming</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='2' name='services[]' value='2' />
+        <label class='form-check-label' for='2'>packpaker Hotels &hospitality</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='3' name='services[]' value='3' />
+        <label class='form-check-label' for='3'>Farming & Homesteads</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='4' name='services[]' value='4' />
+        <label class='form-check-label' for='4'>Building & Restoration</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='5' name='services[]' value='5' />
+        <label class='form-check-label' for='5'>Teaching & language</label>
+      </div>
+      <div class='form-check'>
+        <input type='checkbox' class='form-check-input' id='6' name='services[]' value='6' />
+        <label class='form-check-label' for='6'>intenships Abroad</label>
+      </div>
+
+    <div>
+      <button type="submit" class="main-btn">Filter</button>
+    </div>
+  </form>
+
 </aside>
