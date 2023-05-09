64a003f3eceec3f07ded401a88a96fa12c59fa5a
salahcst344 <salahcst344@gmail.com>
2023-05-10 00:52:09 +0300

    Fix bugs F ali mohamed

--- core/Classes/Host.php
+++ core/Classes/Host.php
@@ -252,32 +252,31 @@ zdmcnsklnsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     public function getOne($id)
     {
         $host = HostDB::getOne($id);
-
         extract($host);
 
         $this->id = $id;
-        $this->firstName = $first_name ;
+        $this->firstName = $first_name;
         $this->lastName = $last_name;
-        $this-> user_name = $user_name ;
+        $this->user_name = $user_name;
         $this->password = $password;
         $this->type = $type;
-        $this ->coverPhoto = $cover_img;
-        $this ->profilePhoto = $profile_img;
+        $this->coverPhoto = $cover_img;
+        $this->profilePhoto = $profile_img;
         $this->email = $email;
         $this->phoneNumber = $phone_num;
         $this->country = $country;
-        $this->notification [] = $notificationIds;
+        $this->notification[] = $notificationIds;
 
         $this->location = $location;
-        $this->status =$Status;
+        $this->status = $Status;
 
 
         $this->description = $Description;
 
-        $this->needs[] = $needs ; ////////////////////////////////////////////////////////
+        $this->needs[] = $needs; ////////////////////////////////////////////////////////
 
         $this->Traveller_num = $Traveller_num;
-        $this->rate =$Rate_average ;
+        $this->rate = $Rate_average;
     }
 
 
