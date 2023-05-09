1e9c6392f10c8106e142e83bcbe3eb910fea9c04
salahcst344 <salahcst344@gmail.com>
2023-05-09 19:35:00 +0300

    Fix bug

--- views/partials/filters.view.php
+++ views/partials/filters.view.php
@@ -1,51 +1,24 @@
 <aside style="gap: 3.2rem;width:10%">
   <form class="d-flex flex-column" style="gap: 3.2rem;">
-    <?=
-    $isHost ?
-      "<div>
+    <div style=<?= $isHost ? "display: none;" : '' ?>>
       <h4 class='mb-3'>Rating</h4>
       <div class='form-check'>
-        <input 
-          type='radio' 
-          class='form-check-input' 
-          id='4OrMore' 
-          name='startRate' 
-          value='4'
-          />
+        <input type='radio' class='form-check-input' id='4OrMore' name='startRate' value='4' />
         <label class='form-check-label' for='4OrMore'>4 or more</label>
       </div>
       <div class='form-check'>
-        <input 
-          type='radio' 
-          class='form-check-input' 
-          id='3OrMore' 
-          name='startRate' 
-          value='3'
-          />
+        <input type='radio' class='form-check-input' id='3OrMore' name='startRate' value='3' />
         <label class='form-check-label' for='3OrMore'>3 or more</label>
       </div>
       <div class='form-check'>
-        <input 
-          type='radio' 
-          class='form-check-input' 
-          id='2OrMore' 
-          name='startRate' 
-          value=2
-          />
+        <input type='radio' class='form-check-input' id='2OrMore' name='startRate' value=2 />
         <label class='form-check-label' for='2OrMore'>2 or more</label>
       </div>
       <div class='form-check'>
-        <input 
-          type='radio' 
-          class='form-check-input' 
-          id='1OrMore' 
-          name='startRate' 
-          value=1
-          />
+        <input type='radio' class='form-check-input' id='1OrMore' name='startRate' value=1 />
         <label class='form-check-label' for='1OrMore'>1 or more</label>
       </div>
-    </div>" : ''
-    ?>
+    </div>
 
     <div>
       <button type="submit" class="main-btn">Filter</button>
