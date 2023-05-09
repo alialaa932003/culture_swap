84cc0931da2076a4b511d8a54fd3834a0fa1648f
shehab <shehabwaleed2010@gmail.com>
2023-05-09 15:24:05 +0300

    SH

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     require base_path($class . ".php");
 });
-
+ 
 
 $router = new \core\Router();
 
