29e8ff773c191bcf0097748e8029ecc32bb3054b
salahcst344 <salahcst344@gmail.com>
2023-05-09 21:27:00 +0300

    Make filters gineric

--- views/partials/filters.view.php
+++ views/partials/filters.view.php
@@ -26,7 +26,7 @@
     <div style=<?= $isHost ? "display: none;" : '' ?>>
       <h4 class='mb-3'>Countries</h4>
 
-      <?php foreach ($allCountries as $i => $country): ?>
+      <?php foreach ($allCountries as $i => $country) : ?>
 
         <div class='form-check'>
           <input type='checkbox' class='form-check-input' id='<?= $i ?>' name='countries[]' value='<?= $country['country'] ?>' />
@@ -36,31 +36,34 @@
     </div>
 
 
+    <?php
+    $serviceNaming = $isHost ? "needs" : "services";
+    ?>
     <div style=<?= $isHost ? "display: none;" : '' ?>>
-      <h4 class='mb-3'>Services</h4>
+      <h4 class='mb-3' style="text-transform: capitalize;"><?= $serviceNaming ?></h4>
 
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='1' name='services[]' value='1' />
+        <input type='checkbox' class='form-check-input' id='1' name='<?= $serviceNaming ?>[]' value='1' />
         <label class='form-check-label' for='1'>Animals & Farming</label>
       </div>
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='2' name='services[]' value='2' />
+        <input type='checkbox' class='form-check-input' id='2' name='<?= $serviceNaming ?>[]' value='2' />
         <label class='form-check-label' for='2'>packpaker Hotels &hospitality</label>
       </div>
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='3' name='services[]' value='3' />
+        <input type='checkbox' class='form-check-input' id='3' name='<?= $serviceNaming ?>[]' value='3' />
         <label class='form-check-label' for='3'>Farming & Homesteads</label>
       </div>
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='4' name='services[]' value='4' />
+        <input type='checkbox' class='form-check-input' id='4' name='<?= $serviceNaming ?>[]' value='4' />
         <label class='form-check-label' for='4'>Building & Restoration</label>
       </div>
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='5' name='services[]' value='5' />
+        <input type='checkbox' class='form-check-input' id='5' name='<?= $serviceNaming ?>[]' value='5' />
         <label class='form-check-label' for='5'>Teaching & language</label>
       </div>
       <div class='form-check'>
-        <input type='checkbox' class='form-check-input' id='6' name='services[]' value='6' />
+        <input type='checkbox' class='form-check-input' id='6' name='<?= $serviceNaming ?>[]' value='6' />
         <label class='form-check-label' for='6'>intenships Abroad</label>
       </div>
     </div>
