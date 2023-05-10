9ef0a99fe562a5be999a69928f9e81bf1b8f510f
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2023-05-10 04:15:02 +0300

    effect

--- views/index.view.php
+++ views/index.view.php
@@ -55,7 +55,7 @@
 
 
                     <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 item">
-                        <a href="/culture_swap/hosts?service=<?= $service["Id"] ?>">
+                        <a href="/culture_swap/hosts?need=<?= $service["Id"] ?>">
 
                             <span class="itemIcon">
 
