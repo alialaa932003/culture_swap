7243bcb2b35d8a1294bc7fd505f61be5a211a053
shehab <shehabwaleed2010@gmail.com>
2023-05-10 04:05:08 +0300

    adding stats

--- views/index.view.php
+++ views/index.view.php
@@ -170,7 +170,7 @@
 
     <!-------------------------------------     end  cards        -------------------------------------------->
 
-    <section class=" statistics">
+    <section class=" statistics" id="stats">
         <div class="container">
             <div class="row">
                 <div class="col-xl-3 col-md-6">
